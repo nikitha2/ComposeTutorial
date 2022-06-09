8d40e8d06ebda90c0c7ebb0a081356edd4793e32
Nikitha Gullapalli <Android2012>
2022-06-09 15:10:08 -0700

    UI- chat app with sample date

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -29,7 +34,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MessageCard(Message("Android","Jetpack compose"))
+                    Conversation(SampleData.conversationSample1)
+
                 }
             }
         }
@@ -37,8 +43,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message->
+            MessageCard(message)
+        }
+    }
 }
 
 data class Message(val author: String, val body:String)
@@ -56,15 +66,38 @@ fun MessageCard(message: Message) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape))
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(text = message.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2)
-            // Add a vertical space between the author and message texts
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = message.body,
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2)
+
+            //Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ){
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 
@@ -81,7 +114,8 @@ fun MessageCard(message: Message) {
 fun DefaultPreview() {
     ComposeTutorialTheme {
         //Greeting("Android")
-        MessageCard(message = Message("Author","Body"))
+        Conversation(SampleData.conversationSample)
+        //MessageCard(message = Message("Author","Body"))
     }
 
 }
