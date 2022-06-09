f02eee5ce7df2a0b15b21609a2bf5c4228a220a2
Nikitha Gullapalli <Android2012>
2022-06-08 23:04:33 -0700

    preview for dark and light mode

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -12,7 +12,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -70,13 +69,19 @@ fun MessageCard(message: Message) {
     }
 
 }
-
-@Preview(showBackground = true)
+//preview light mode
+@Preview(name = "Light Mode",showBackground = true)
+//preview dark mode
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
     ComposeTutorialTheme {
         //Greeting("Android")
-        MessageCard(message = Message("author","body"))
+        MessageCard(message = Message("Author","Body"))
     }
 
 }
